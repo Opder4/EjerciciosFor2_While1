68256f51d6dd5e4fefffb345760a16237bbba65c
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 13:32:52 -0600

    Fin Ejercicio 25

--- Ejercicio 25/Ejercicio 25/Program.cs
+++ Ejercicio 25/Ejercicio 25/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿int opcion = 0;
+while (opcion!=3)
+{
+    Console.WriteLine(" Menú ");
+    Console.WriteLine();
+    Console.WriteLine("1. Salduo");
+    Console.WriteLine("2. Consejo");
+    Console.WriteLine("3. Salir");
+    Console.WriteLine();
+    opcion = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Hola");
+            break;
+        case 2:
+            Console.WriteLine("Sigue adelante");
+            break;
+        case 3:
+                Console.WriteLine("Saliendo");
+            break;
+        default:
+            Console.WriteLine("Opción no válida");
+            break;
+    }
+    Console.WriteLine();
+    Console.ReadLine();
+    Console.Clear();
+}
+Console.WriteLine("Programa finalizado");
