aa84727ba7a633d97f05ff287b2cc2b47cdaee85
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 10:29:50 -0600

    Fin Ejercicio 16

--- Ejercicio 16/Ejercicio 16/Program.cs
+++ Ejercicio 16/Ejercicio 16/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿int numero = 1;
+while (numero!=0)
+{
+    Console.WriteLine("Ingrese un número");
+    numero=int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Usted Ingreso 0, programa terminado");
