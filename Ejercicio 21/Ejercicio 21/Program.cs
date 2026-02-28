f51cb14c0f196809a3ace1966391032b24216613
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 13:01:54 -0600

    Fin Ejercicio 21

--- Ejercicio 21/Ejercicio 21/Program.cs
+++ Ejercicio 21/Ejercicio 21/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int suma = 0;
+while (suma<=100)
+{
+    Console.WriteLine("Ingrese un número para sumar: ");
+    Console.WriteLine();
+    int numero=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    suma += numero;
+}
+Console.WriteLine("La suma llegó al límite de 100");
