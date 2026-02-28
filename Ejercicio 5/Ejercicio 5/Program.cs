194a8488a7422b1aeaac1b718fbe3c9dc88b48c9
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 09:33:28 -0600

    Fin Ejercicio 5

--- Ejercicio 5/Ejercicio 5/Program.cs
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿for (int i=1;i<=5;i++)
+{
+    Console.WriteLine($"La tabla del {i}");
+    for (int z=1; z<=5;z++)
+    {
+        Console.WriteLine($"{i} X {z} = {i*z}");
+    }
+    Console.WriteLine();
+}
