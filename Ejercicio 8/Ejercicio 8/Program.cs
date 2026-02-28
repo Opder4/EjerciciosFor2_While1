1f801bc08ff3169cf3c583629569bf3135491b69
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 10:04:49 -0600

    Fin Ejercicio 8

--- Ejercicio 8/Ejercicio 8/Program.cs
+++ Ejercicio 8/Ejercicio 8/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int numero = 0;
+for (int i = 1; i <= 4; i++)
+{
+    for (int z=1; z<=4; z++)
+    {
+        Console.Write($" {z+numero}");
+    }
+    Console.WriteLine();
+    numero += 1;
+}
