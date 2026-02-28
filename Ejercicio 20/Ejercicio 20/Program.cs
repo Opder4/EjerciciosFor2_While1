262f0ea04c67103d4910ce7e92cd751f7b9852a3
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 12:57:00 -0600

    Fin Ejercicio 20

--- Ejercicio 20/Ejercicio 20/Program.cs
+++ Ejercicio 20/Ejercicio 20/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int contador = 0;
+int resta = 1;
+Console.WriteLine("Ingrese un número: ");
+Console.WriteLine();
+int numero=int.Parse(Console.ReadLine());
+int numeroreferencia = numero;
+Console.WriteLine();
+while (numero>=0)
+{
+    numero = numeroreferencia - resta;
+    resta = resta * 10;
+    if (numero>=0 || numero==-1)
+    {
+        contador = contador + 1;
+    }
+}
+
+Console.WriteLine($"El número tiene {contador} dígitos");
