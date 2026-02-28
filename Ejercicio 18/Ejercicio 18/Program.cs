b21f697512ba791465a0fda8d6376791eec30fd6
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 11:40:24 -0600

    Fin Ejercicio 18

--- Ejercicio 18/Ejercicio 18/Program.cs
+++ Ejercicio 18/Ejercicio 18/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System.Numerics;
+using System.Threading.Channels;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+int cambiante = 1;
+Console.WriteLine();
+Console.WriteLine("Ingrese un número");
+Console.WriteLine();
+int numero = int.Parse(Console.ReadLine());
+int numeroreferencia = numero;
+Console.WriteLine();
+while (numero>0 && cambiante<numeroreferencia)
+{
+    numero = numero*(numeroreferencia - cambiante);
+    cambiante++;
+}
+Console.WriteLine($"El factorial es {numero}");
