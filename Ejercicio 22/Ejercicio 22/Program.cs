7f9f5e8496e2095ad4fe6f772c978ed219c4ca9f
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 13:13:11 -0600

    Fin Ejercicio 22

--- Ejercicio 22/Ejercicio 22/Program.cs
+++ Ejercicio 22/Ejercicio 22/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿string contra = "";
+while (contra!="abcd")
+{
+    Console.WriteLine("Ingrese la contraseña: ");
+    Console.WriteLine();
+    contra=Console.ReadLine();
+    Console.WriteLine();
+    Console.Clear();
+}
+Console.WriteLine("La contraseña es correcta");
+Console.WriteLine();
+Console.WriteLine("Puede proseguir");
