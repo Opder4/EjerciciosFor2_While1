5f209871b8a455515ab2e3ab750ec6daea8fd128
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 10:36:12 -0600

    Fin Ejercicio 17

--- Ejercicio 17/Ejercicio 17/Program.cs
+++ Ejercicio 17/Ejercicio 17/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int numero = 1;
+int suma = 0;
+Console.WriteLine("Presione 0 para terminar");
+while (numero!=0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Ingrese un número");
+    Console.WriteLine();
+    numero=int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    if (numero>0)
+    {
+        suma += 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Usted salio");
+Console.WriteLine();
+Console.WriteLine($"La cantidad de números positivos que ingreso son {suma}");
