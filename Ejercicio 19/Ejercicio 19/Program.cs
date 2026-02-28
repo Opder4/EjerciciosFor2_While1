6dbbcb4bbd14d7e1654bbfff9aebc8499fbadfed
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 11:46:25 -0600

    Fin Ejercicio 19

--- Ejercicio 19/Ejercicio 19/Program.cs
+++ Ejercicio 19/Ejercicio 19/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿int multiplo = 1;
+Console.WriteLine();
+Console.WriteLine("Ingrese un número");
+Console.WriteLine();
+int numero=int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Tabla del {numero}");
+Console.WriteLine();
+while (multiplo<=10)
+{
+    Console.WriteLine($"{numero} X {multiplo} = {numero*multiplo}");
+    multiplo++;
+}
