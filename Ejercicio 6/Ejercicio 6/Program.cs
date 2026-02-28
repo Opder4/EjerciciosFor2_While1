b56f615489efa30182e7c41860c3aa85fd32c88f
Emanuel Ramírez <er6348100@gmail.com>
2026-02-28 09:49:17 -0600

    Fin Ejercicio 6

--- Ejercicio 6/Ejercicio 6/Program.cs
+++ Ejercicio 6/Ejercicio 6/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿int numero = 0;
+for (int i=1; i<=3; i++)
+{
+    Console.Write("|");
+    for (int z=1;z<=3;z++)
+    {
+        Console.Write($" {z+numero }");
+    }
+    Console.WriteLine(" |");
+    numero += 3;
+}
